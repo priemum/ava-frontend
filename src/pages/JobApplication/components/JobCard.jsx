e417569d447716ac4fabda03ddf55ac20fcb91e3
moazabdulhalim <moazabdulhalim@gmail.com>
2023-10-22 02:11:57 +0300

    change expired design

--- src/pages/JobApplication/components/JobCard.jsx
+++ src/pages/JobApplication/components/JobCard.jsx
@@ -51,11 +51,11 @@ const JobCard = ({
               </div>
             </div>
           </div>
-          {Expired && (
+          {/* {Expired && (
             <div className="absolute -rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               <p className="text-red-500 font-bold text-med">EXPIRED</p>
             </div>
-          )}
+          )} */}
           <div className="flex justify-center items-center gap-x-4">
             <Button
               borderColor={"border-primary"}
