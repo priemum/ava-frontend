92509a1668b30074cf608325ff91cea890400ea0
Kanaan Alahmad <kanaan92@hotmail.com>
2023-12-18 16:47:56 +0400

    K-Tranlsations-v2.0

--- src/data/aboutUsData.js
+++ src/data/aboutUsData.js
@@ -11,106 +11,115 @@ import facesIcon from "../assets/icons/faces.svg";
 import developmentIcon from "../assets/icons/development.svg";
 import Logo from "../assets/logos/black-logo.svg";
 export const data = {
-  headerTitle: { en: "Get to know us closely and what we excel at", ar: "" },
-  Goals: { en: "Our goal is to provide", ar: "" },
-  GoalsSub: { en: "best offers for homes in Dubai", ar: "" },
-  searchSlogan: { en: "There is a new way to search for a house", ar: "" },
-  mission: {
-    title: { en: "Mission", ar: "" },
-    description: {
-      en: "our mission is to provide exceptional professional services to our clients with integrity, transparency, and excellence.",
-      ar: "",
-    },
-    image: dubai,
-    items: [
-      {
-        icon: teamWorkIcon,
-        title: { en: "Trust", ar: "" },
-        description: {
-          en: "We are dedicated to providing a positive experience and to earning the trust and confidence of our clients. We strive to deliver on our promises and to achieve the best possible outcomes for our clients.",
-          ar: "",
-        },
-      },
-      {
-        icon: projectsIcon,
-        title: { en: "Leading", ar: "" },
-        description: {
-          en: "We aim to be recognized as a leading real estate company in Dubai by offering a comprehensive range of services such as buying, selling, renting, and managing properties. We ensure that our clients’ needs are met with the utmost diligence and attention to detail.",
-          ar: "",
-        },
-      },
-      {
-        icon: developmentIcon,
-        title: { en: "Solutions", ar: "" },
-        description: {
-          en: "Our mission is to provide customized solutions to our clients and to exceed their expectations. ",
-          ar: "",
-        },
-      },
-      {
-        icon: facesIcon,
-        title: { en: "Diversity", ar: "" },
-        description: {
-          en: "We strive to meet the diverse needs of our clients and to establish a lasting relationship based on trust and mutual respect.",
-          ar: "",
-        },
-      },
-    ],
-  },
-  vision: {
-    title: { en: "Vision", ar: "" },
-    description: {
-      en: "At AVA Real Estate, our vision is to be The Best Real Estate Company in the industry",
-      ar: "",
-    },
-    imageOne: about330,
-    imageTwo: aboutAmber,
-    imageThree: aboutJLT,
-    items: [
-      {
-        text: {
-          en: "Both in the minds of people and the communities. We expect and believe in delivering The Best we have to offer by always keeping our values at the top without compromising customer values.",
-          ar: "",
-        },
-      },
-      {
-        text: {
-          en: "We want to create The Best working environment where everyone has equal opportunity to grow and be proud of working together for the growth of the communities and the nation.",
-          ar: "",
-        },
-      },
-      {
-        text: {
-          en: "|Our vision is supported by two fundamental principles that provide the foundation of all our activities: Organizational Excellence and the Customer Values.",
-          ar: "",
-        },
-      },
-      {
-        text: {
-          en: "Our vision tells the world-what we care about; our clients, communities and the employees. We believe in building life-long relationships with Our clients.",
-          ar: "",
-        },
-      },
-    ],
-  },
-  founnders: [
-    {
-      image: kasra,
-      name: { en: "Kasra Malakouti", ar: "" },
-      title: { en: "PRESIDENT & DIRECTOR", ar: "" },
-      description: {
-        en: "Dubai is a vibrant city with a rapidly growing real estate market, and we are fortunate to be part of this exciting time. Our focus will be on delivering exceptional customer experiences, enhancing our portfolio, and increasing our market share through innovative marketing strategies. We will continue to hire and retain the best talent who share our vision and passion for excellence. We recognize that our success is dependent on our unwavering commitment to the highest standards of professionalism, ethics, and corporate responsibility.",
-        ar: "",
-      },
-    },
-    {
-      image: maryam,
-      name: { en: "Maryam Pourgolam", ar: "" },
-      title: { en: "CHIEF EXECUTIVE OFFICER", ar: "" },
-      description: {
-        en: "Dubai is a vibrant city with a rapidly growing real estate market, and we are fortunate to be part of this exciting time. Our focus will be on delivering exceptional customer experiences, enhancing our portfolio, and increasing our market share through innovative marketing strategies. We will continue to hire and retain the best talent who share our vision and passion for excellence. We recognize that our success is dependent on our unwavering commitment to the highest standards of professionalism, ethics, and corporate responsibility",
-        ar: "",
-      },
-    },
-  ],
+	headerTitle: {
+		en: "Get to know us closely and what we excel at",
+		ar: "تعرف علينا عن قرب وعلى ما نتميز به",
+	},
+	Goals: { en: "Our goal is to provide", ar: "هدفنا هو تقديم" },
+	GoalsSub: {
+		en: "best offers for homes in Dubai",
+		ar: "أفضل العروض للسكن في دبي",
+	},
+	searchSlogan: {
+		en: "There is a new way to search for a house",
+		ar: "طريقة جديدة للبحث عن المنزل",
+	},
+	mission: {
+		title: { en: "Mission", ar: "المهمة" },
+		description: {
+			en: "our mission is to provide exceptional professional services to our clients with integrity, transparency, and excellence.",
+			ar: "نعمل في شركة آفا العقارية على تقديم خدمات استثنائئية و احترافية لعملائنا تتسم بالشفافية و النزاهة و التميز",
+		},
+		image: dubai,
+		items: [
+			{
+				icon: teamWorkIcon,
+				title: { en: "Trust", ar: "الثقة" },
+				description: {
+					en: "We are dedicated to providing a positive experience and to earning the trust and confidence of our clients. We strive to deliver on our promises and to achieve the best possible outcomes for our clients.",
+					ar: "نسعى دائما لتقديم أفضل تجربة لعملائنا و لكسب ثقتهم و ذلك من خلال الوفاء بوعودنا و تحقيق أفضل نتائج ممكنة للعملاء.",
+				},
+			},
+			{
+				icon: projectsIcon,
+				title: { en: "Leading", ar: "قيادة" },
+				description: {
+					en: "We aim to be recognized as a leading real estate company in Dubai by offering a comprehensive range of services such as buying, selling, renting, and managing properties. We ensure that our clients’ needs are met with the utmost diligence and attention to detail.",
+					ar: "نهدف إلى أن يتم الاعتراف بنا كشركة عقارية رائدة في دبي من خلال تقديم مجموعة شاملة من الخدمات مثل شراء وبيع وتأجير وإدارة العقارات. نحن نضمن تلبية احتياجات عملائنا بأقصى درجات العناية والاهتمام بالتفاصيل.",
+				},
+			},
+			{
+				icon: developmentIcon,
+				title: { en: "Solutions", ar: "حلول" },
+				description: {
+					en: "Our mission is to provide customized solutions to our clients and to exceed their expectations. ",
+					ar: "مهمتنا هي تقديم حلول مخصصة لعملائنا وتجاوز توقعاتهم.",
+				},
+			},
+			{
+				icon: facesIcon,
+				title: { en: "Diversity", ar: "تنوع" },
+				description: {
+					en: "We strive to meet the diverse needs of our clients and to establish a lasting relationship based on trust and mutual respect.",
+					ar: "نحن نسعى جاهدين لتلبية الاحتياجات المتنوعة لعملائنا وإقامة علاقة دائمة مبنية على الثقة والاحترام المتبادل.",
+				},
+			},
+		],
+	},
+	vision: {
+		title: { en: "Vision", ar: "رؤيتنا" },
+		description: {
+			en: "At AVA Real Estate, our vision is to be The Best Real Estate Company in the industry",
+			ar: "رؤيتنا هي تقديم أفضل حلول عقارية ",
+		},
+		imageOne: about330,
+		imageTwo: aboutAmber,
+		imageThree: aboutJLT,
+		items: [
+			{
+				text: {
+					en: "Both in the minds of people and the communities. We expect and believe in delivering The Best we have to offer by always keeping our values at the top without compromising customer values.",
+					ar: "سواء للمجتمع او للافراد, نعمل على تقديم افضل ما لدينا من خلال المحافظة على قيمنا و قيم عملائنا",
+				},
+			},
+			{
+				text: {
+					en: "We want to create The Best working environment where everyone has equal opportunity to grow and be proud of working together for the growth of the communities and the nation.",
+					ar: "نريد إنشاء أفضل بيئة عمل حيث يتمتع الجميع بفرصة متساوية للنمو ويفخرون بالعمل معًا من أجل نمو المجتمعات والأمة.",
+				},
+			},
+			{
+				text: {
+					en: "Our vision is supported by two fundamental principles that provide the foundation of all our activities: Organizational Excellence and the Customer Values.",
+					ar: "رؤيتنا مبنية على مبدأين أساسيين هما التميز في التنظيم و الاهتمام بقيم العملاء",
+				},
+			},
+			{
+				text: {
+					en: "Our vision tells the world-what we care about; our clients, communities and the employees. We believe in building life-long relationships with Our clients.",
+					ar: "تدعم رؤيتنا ما نهتم به, عملائنا و المجتمع و العاملون لدينا. نهتم من خلالها ببناء علاقة طويلة الامد مع عملائنا",
+				},
+			},
+		],
+	},
+	founnders: [
+		{
+			image: kasra,
+			name: { en: "Kasra Malakouti", ar: "كسرى ملكوتي" },
+			title: { en: "DIRECTOR", ar: "مدير" },
+			description: {
+				en: "Dubai is a vibrant city with a rapidly growing real estate market, and we are fortunate to be part of this exciting time. Our focus will be on delivering exceptional customer experiences, enhancing our portfolio, and increasing our market share through innovative marketing strategies. We will continue to hire and retain the best talent who share our vision and passion for excellence. We recognize that our success is dependent on our unwavering commitment to the highest standards of professionalism, ethics, and corporate responsibility.",
+				ar: "تعد دبي مدينة دبي من أكثر المدن النابضة في الحياة حول العالم و تتمتع بسوق عقاري سريع النمو. نحن محظوظون لكوننا جزء من هذا النمو المثير. نعمل في آفا العقارية على تقديم تجارب استثنائية للعملاء و تعزيز مكاننا و زيادة حصتنا في السوق و ذلك من خلال اعتماد استراتيجيات تسويق مبتكرة. يتجسد ذلك من خلال توظيف أفضل المواهب التي تشاركنا في الرؤية و الاحتفاظ بها. إننا ندرك ان نجاحنا يعتمد بشكل أساسي على التزامنا بأعلى معايير الاحتراف و الاخلاق و المسؤولية.",
+			},
+		},
+		{
+			image: maryam,
+			name: { en: "Maryam Pourgolam", ar: "مريم بورغلام" },
+			title: { en: "CHIEF EXECUTIVE OFFICER", ar: "المدير التنفيذي" },
+			description: {
+				en: "Dubai is a vibrant city with a rapidly growing real estate market, and we are fortunate to be part of this exciting time. Our focus will be on delivering exceptional customer experiences, enhancing our portfolio, and increasing our market share through innovative marketing strategies. We will continue to hire and retain the best talent who share our vision and passion for excellence. We recognize that our success is dependent on our unwavering commitment to the highest standards of professionalism, ethics, and corporate responsibility",
+				ar: "تعد دبي مدينة دبي من أكثر المدن النابضة في الحياة حول العالم و تتمتع بسوق عقاري سريع النمو. نحن محظوظون لكوننا جزء من هذا النمو المثير. نعمل في آفا العقارية على تقديم تجارب استثنائية للعملاء و تعزيز مكاننا و زيادة حصتنا في السوق و ذلك من خلال اعتماد استراتيجيات تسويق مبتكرة. يتجسد ذلك من خلال توظيف أفضل المواهب التي تشاركنا في الرؤية و الاحتفاظ بها. إننا ندرك ان نجاحنا يعتمد بشكل أساسي على التزامنا بأعلى معايير الاحتراف و الاخلاق و المسؤولية.",
+			},
+		},
+	],
 };
