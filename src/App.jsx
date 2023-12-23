f9b7a997eea6b7d3beb1fd323c85e95814c1c919
Kanaan Alahmad <kanaan92@hotmail.com>
2023-12-23 15:09:18 +0400

    K-SEO-v1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import Router from "./routes/index";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+ReactGA.initialize("G-TKD3LN2FB4");
 const App = () => {
-  return <Router />;
+	const location = useLocation();
+	useEffect(() => {
+		ReactGA.pageview(location.pathname + location.search);
+	}, [location]);
+	return <Router />;
 };
 export default App;
