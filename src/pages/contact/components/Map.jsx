de8f0789f75c27dce780de689f87b72f7028102c
moazabdulhalim <moazabdulhalim@gmail.com>
2024-01-25 14:29:44 +0300

    design fix

--- src/pages/contact/components/Map.jsx
+++ src/pages/contact/components/Map.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const ContactMap = () => {
   return (
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14441.703341395783!2d55.2696918!3d25.188858!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6965bad345df%3A0x97dc6af8848637a8!2sAva%20Real%20Estate!5e0!3m2!1sen!2s!4v1697820982359!5m2!1sen!2s"
-      width="100%"
-      height="100%"
-      className="border-none !z-20"
-      allowFullScreen=""
-      loading="lazy"
-      referrerPolicy="no-referrer-when-downgrade"
-    ></iframe>
+    <div className="bg-white/50 h-[430px] w-full rounded-md overflow-hidden">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14441.703341395783!2d55.2696918!3d25.188858!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6965bad345df%3A0x97dc6af8848637a8!2sAva%20Real%20Estate!5e0!3m2!1sen!2s!4v1697820982359!5m2!1sen!2s"
+        width="100%"
+        height="100%"
+        className="border-none !z-20"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
   );
 };
 
