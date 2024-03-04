d1bf85e2bb25d8d98fcd89a7f186912fa21e771d
moazabdulhalim <moazabdulhalim@gmail.com>
2024-03-04 16:01:51 +0300

    fix message style (Alert)

--- src/components/UI/Message/index.jsx
+++ src/components/UI/Message/index.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 const MessageBox = () => {
   const dispatch = useDispatch();
   const state = useSelector(selectState);
+
   const options = useSelector(selectOptions);
   useEffect(() => {
     if (state) {
@@ -22,7 +23,7 @@ const MessageBox = () => {
     <div
       className={`${
         state ? "scale-100 opacity-100" : "scale-0 opacity-0"
-      } transition-all duration-300 h-[100px] w-[80vw] max-w-[500px] shadow-2xl drop-shadow-2xl rounded-md fixed top-[10vh] left-1/2 -translate-x-1/2 z-50 p-4 flex items-center justify-between`}
+      } transition-all duration-300  w-[80vw] max-w-[500px] shadow-2xl drop-shadow-2xl rounded-md fixed top-[10vh] left-1/2 -translate-x-1/2 z-50 p-4 flex items-center justify-between`}
       style={{
         background:
           options.variant === "success"
