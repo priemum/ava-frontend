dbe05f1047bccddc50c838a385cdf68e524f1214
Kanaan Alahmad <kanaan92@hotmail.com>
2024-03-12 11:16:26 +0400

    K-attendees

--- src/pages/openHouse/index.jsx
+++ src/pages/openHouse/index.jsx
@@ -15,11 +15,12 @@ import { showMessage } from "../../redux/messageAction.slice";
 import { useAddOpenHouseMutation } from "../../redux/openHouse/openHouseSlice";
 
 const defaultFormState = {
-  Email: "",
-  FullName: "",
-  PhoneNo: "",
-  Agent: "",
-  //   PreferredTime: "",
+	Email: "",
+	FullName: "",
+	PhoneNo: "",
+	Agent: "",
+	Attendees: 1,
+	//   PreferredTime: "",
 };
 const agentsList = ["Aika", "Fadl", "Maryam", "Kasra", "Marwa"];
 // const timeList = [
@@ -28,185 +29,197 @@ const agentsList = ["Aika", "Fadl", "Maryam", "Kasra", "Marwa"];
 //   "14:00 P.M - 16:00 P.M",
 // ];
 const OpenHousePage = () => {
-  const { t, i18n } = useTranslation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const {
-    disabled,
-    setErrors,
-    errors,
-    setValues,
-    values,
-    handleChange,
-    handleSubmit,
-  } = useForm(submit, defaultFormState);
-  const [addOpenHouse, { isLoading, isSuccess, isError }] =
-    useAddOpenHouseMutation();
-  const form = useRef();
+	const { t, i18n } = useTranslation();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const {
+		disabled,
+		setErrors,
+		errors,
+		setValues,
+		values,
+		handleChange,
+		handleSubmit,
+	} = useForm(submit, defaultFormState);
+	const [addOpenHouse, { isLoading, isSuccess, isError }] =
+		useAddOpenHouseMutation();
+	const form = useRef();
 
-  const sendEmail = (e) => {
-    emailjs
-      .sendForm(
-        "service_b8ayq22",
-        "template_xqjgpmg",
-        form.current,
-        "sxh5TJan60LQqD6Sw"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-  async function submit(e) {
-    let formData = new FormData(form.current);
-    try {
-      const response = await fetch(import.meta.env.VITE_ZAPPIER_URL, {
-        method: "POST",
-        body: formData,
-        "Content-Type": "multipart/form-data",
-      });
-      const result = response.json();
-      // console.log("Success:", result);
-      sendEmail(e);
-      addOpenHouse({ values });
+	const sendEmail = (e) => {
+		emailjs
+			.sendForm(
+				"service_b8ayq22",
+				"template_xqjgpmg",
+				form.current,
+				"sxh5TJan60LQqD6Sw",
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+				},
+				(error) => {
+					console.log(error.text);
+				},
+			);
+	};
+	async function submit(e) {
+		let formData = new FormData(form.current);
+		try {
+			const response = await fetch(import.meta.env.VITE_ZAPPIER_URL, {
+				method: "POST",
+				body: formData,
+				"Content-Type": "multipart/form-data",
+			});
+			const result = response.json();
+			// console.log("Success:", result);
+			sendEmail(e);
+			addOpenHouse({ values });
 
-      dispatch(
-        showMessage({
-          variant: "success",
-          message: t("SubmitThanks"),
-        })
-      );
-      navigate("/");
-    } catch (error) {
-      console.error("Error here:", error);
-    }
-  }
-  return (
-    <div
-      className="h-full min-h-screen w-full bg-center bg-cover bg-no-repeat flex flex-col justify-start items-center py-24 max-lg:space-y-7 lg:space-y-4 "
-      style={{
-        backgroundImage: `url(${BGBACK})`,
-      }}
-    >
-      <img
-        src={LOGO}
-        alt="LOGO"
-        className="h-[100px] w-[100px] lg:h-[150px] lg:w-[150px]"
-      />
-      <p className="text-[#5D473F] text-[35px] lg:text-big font-extralight text-center">
-        {t("OpenHouseTitle")}
-      </p>
-      <form
-        ref={form}
-        onSubmit={handleSubmit}
-        className="border-2 border-black/50 rounded-2xl bg-[#CDC6BC]/50 w-full max-w-[90%] lg:max-w-[60%] p-12 grid md:grid-cols-2 gap-5 lg:gap-12 relative"
-      >
-        <div className="space-y-3 relative">
-          <CustomInput
-            inputLabel={t("formFullName")}
-            containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
-            type="text"
-            name="FullName"
-            id="FullName"
-            value={values.FullName}
-            onChange={handleChange}
-            error={Boolean(errors?.FullName)}
-          />
-        </div>
-        <div className="space-y-3 relative">
-          <p className="font-semibold leading-3 translate-y-2 px-1">
-            {t("formPhoneNumber")}
-          </p>
-          <PhoneInput
-            country={"ae"}
-            placeholder={t("formPhoneNumber")}
-            enableSearch={true}
-            inputProps={{
-              name: "phone",
-              id: "phone",
-              required: true,
-            }}
-            onChange={(phone) => {
-              if (phone.length < 10) {
-                setErrors({
-                  ...errors,
-                  PhoneNo: "Phone Number is atleast 10 digits",
-                });
-              } else {
-                let newObj = omit(errors, "PhoneNo");
-                setErrors(newObj);
-              }
-              setValues({ ...values, PhoneNo: phone });
-            }}
-            value={values.PhoneNo}
-            containerStyle={{
-              outline: "none",
-              outlineOffset: "0px",
-              boxShadow: "none",
-            }}
-            containerClass={`${
-              Boolean(errors.PhoneNo)
-                ? "!border-red-500 !border-[2px]"
-                : "border-black border-b-[1px]"
-            } px-1  flex bg-white/0 !outline-none`}
-            inputClass={`!bg-transparent !text-black !w-full !text-lg !h-full !border-none ${
-              i18n.language == "en" ? "px-0" : "mx-10"
-            } !outline-none`}
-            buttonClass={`!border-none !outline-none !text-lg `}
-            buttonStyle={{
-              direction: "ltr",
-              outline: "none",
-              outlineOffset: "0px",
-              boxShadow: "none",
-            }}
-            dropdownClass="!bg-primary/90 !text-secondary"
-            searchClass="!bg-primary/90 !text-secondary"
-            inputStyle={{
-              direction: "ltr",
-              outline: "none",
-              outlineOffset: "0px",
-              boxShadow: "none",
-            }}
-          />
-        </div>
-        <div className="space-y-3 relative">
-          <CustomInput
-            inputLabel={t("formEmail")}
-            containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
-            type="email"
-            name="Email"
-            id="Email"
-            value={values.Email}
-            onChange={handleChange}
-            error={Boolean(errors?.Email)}
-          />
-        </div>
-        <div className="space-y-3 relative">
-          <CustomInput
-            inputLabel={t("yourAgent")}
-            placeholder={t("SelectAgent")}
-            containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
-            customStyle={"placeholder:!text-black"}
-            type="text"
-            name="Agent"
-            id="Agent"
-            value={values.Agent}
-            onChange={handleChange}
-            error={Boolean(errors?.Agent)}
-            options={agentsList}
-            state={values}
-            setState={setValues}
-            select
-            readOnly
-            icon={<MdExpandMore size={24} className="text-black" />}
-            reverseIcon
-          />
-        </div>
-        {/* <div className="space-y-3 relative">
+			dispatch(
+				showMessage({
+					variant: "success",
+					message: t("SubmitThanks"),
+				}),
+			);
+			navigate("/");
+		} catch (error) {
+			console.error("Error here:", error);
+		}
+	}
+	return (
+		<div
+			className='h-full min-h-screen w-full bg-center bg-cover bg-no-repeat flex flex-col justify-start items-center py-24 max-lg:space-y-7 lg:space-y-4 '
+			style={{
+				backgroundImage: `url(${BGBACK})`,
+			}}
+		>
+			<img
+				src={LOGO}
+				alt='LOGO'
+				className='h-[100px] w-[100px] lg:h-[150px] lg:w-[150px]'
+			/>
+			<p className='text-[#5D473F] text-[35px] lg:text-big font-extralight text-center'>
+				{t("OpenHouseTitle")}
+			</p>
+			<form
+				ref={form}
+				onSubmit={handleSubmit}
+				className='border-2 border-black/50 rounded-2xl bg-[#CDC6BC]/50 w-full max-w-[90%] lg:max-w-[60%] p-12 grid md:grid-cols-2 gap-5 lg:gap-12 relative'
+			>
+				<div className='space-y-3 relative'>
+					<CustomInput
+						inputLabel={t("formFullName")}
+						containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
+						type='text'
+						name='FullName'
+						id='FullName'
+						value={values.FullName}
+						onChange={handleChange}
+						error={Boolean(errors?.FullName)}
+					/>
+				</div>
+				<div className='space-y-3 relative'>
+					<p className='font-semibold leading-3 translate-y-2 px-1'>
+						{t("formPhoneNumber")}
+					</p>
+					<PhoneInput
+						country={"ae"}
+						placeholder={t("formPhoneNumber")}
+						enableSearch={true}
+						inputProps={{
+							name: "phone",
+							id: "phone",
+							required: true,
+						}}
+						onChange={(phone) => {
+							if (phone.length < 10) {
+								setErrors({
+									...errors,
+									PhoneNo: "Phone Number is atleast 10 digits",
+								});
+							} else {
+								let newObj = omit(errors, "PhoneNo");
+								setErrors(newObj);
+							}
+							setValues({ ...values, PhoneNo: phone });
+						}}
+						value={values.PhoneNo}
+						containerStyle={{
+							outline: "none",
+							outlineOffset: "0px",
+							boxShadow: "none",
+						}}
+						containerClass={`${
+							Boolean(errors.PhoneNo)
+								? "!border-red-500 !border-[2px]"
+								: "border-black border-b-[1px]"
+						} px-1  flex bg-white/0 !outline-none`}
+						inputClass={`!bg-transparent !text-black !w-full !text-lg !h-full !border-none ${
+							i18n.language == "en" ? "px-0" : "mx-10"
+						} !outline-none`}
+						buttonClass={`!border-none !outline-none !text-lg `}
+						buttonStyle={{
+							direction: "ltr",
+							outline: "none",
+							outlineOffset: "0px",
+							boxShadow: "none",
+						}}
+						dropdownClass='!bg-primary/90 !text-secondary'
+						searchClass='!bg-primary/90 !text-secondary'
+						inputStyle={{
+							direction: "ltr",
+							outline: "none",
+							outlineOffset: "0px",
+							boxShadow: "none",
+						}}
+					/>
+				</div>
+				<div className='space-y-3 relative'>
+					<CustomInput
+						inputLabel={t("formEmail")}
+						containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
+						type='email'
+						name='Email'
+						id='Email'
+						value={values.Email}
+						onChange={handleChange}
+						error={Boolean(errors?.Email)}
+					/>
+				</div>
+				<div className='space-y-3 relative'>
+					<CustomInput
+						inputLabel={t("yourAgent")}
+						placeholder={t("SelectAgent")}
+						containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
+						customStyle={"placeholder:!text-black"}
+						type='text'
+						name='Agent'
+						id='Agent'
+						value={values.Agent}
+						onChange={handleChange}
+						error={Boolean(errors?.Agent)}
+						options={agentsList}
+						state={values}
+						setState={setValues}
+						select
+						readOnly
+						icon={<MdExpandMore size={24} className='text-black' />}
+						reverseIcon
+					/>
+				</div>
+				<div className='space-y-3 relative'>
+					<CustomInput
+						inputLabel={t("formAttendees")}
+						containerStyle={"bg-white/0 !border-b-black !rounded-none !py-3"}
+						type='number'
+						name='Attendees'
+						id='Attendees'
+						value={values.Attendees}
+						onChange={handleChange}
+						error={Boolean(errors?.Attendees)}
+					/>
+				</div>
+				{/* <div className="space-y-3 relative">
           <CustomInput
             inputLabel={t("PreferredTime")}
             placeholder={t("SelectTime")}
@@ -227,15 +240,15 @@ const OpenHousePage = () => {
             reverseIcon
           />
         </div> */}
-        <button
-          type="submit"
-          className="h-[60px] w-[150px] bg-[#EDE9E5] rounded-full border-black border-2 absolute -bottom-[30px] right-[5%] z-10 font-semibold transition-all duration-300 hover:bg-[#EDE9E5]/50"
-        >
-          {isLoading ? t("sending") : t("Submit")}
-        </button>
-      </form>
-    </div>
-  );
+				<button
+					type='submit'
+					className='h-[60px] w-[150px] bg-[#EDE9E5] rounded-full border-black border-2 absolute -bottom-[30px] right-[5%] z-10 font-semibold transition-all duration-300 hover:bg-[#EDE9E5]/50'
+				>
+					{isLoading ? t("sending") : t("Submit")}
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default OpenHousePage;
